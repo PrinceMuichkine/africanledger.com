7dc2d81c0e06d4a22cd6ba71f5748d3bfd146b02
PrinceMuichkine <babacar.diop.bba@edhec.com>
2024-08-08 23:10:31 +0000

    cleaning

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+      },
+    ],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
