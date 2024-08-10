7a8ecf365e3f95d71cb0df21f98afe8e44613f49
PrinceMuichkine <babacar.diop.bba@edhec.com>
2024-08-10 18:46:58 +0000

    commit

---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { Button } from '@/components/custom/button'
+
+export default function NotFound() {
+    return (
+        <div className='h-screen'>
+            <div className='m-auto flex h-full w-full flex-col items-center justify-center gap-2'>
+                <h1 className='text-[7rem] font-bold leading-tight'>404</h1>
+                <span className='font-medium'>Oops! Page Not Found!</span>
+                <p className='text-center text-muted-foreground'>
+                    It seems like the page you're looking for <br />
+                    does not exist or might have been removed.
+                </p>
+                <div className='mt-6 flex gap-4'>
+                    <Button variant='outline' onClick={() => window.history.back()}>
+                        Go Back
+                    </Button>
+                    <Link href="/" passHref>
+                        <Button>Back to Home</Button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
