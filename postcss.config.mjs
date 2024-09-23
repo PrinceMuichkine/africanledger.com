8ab9f5f7fc76b5c16d6181101630dc3506aa2e8f
PrinceMuichkine <babacar.diop.bba@edhec.com>
2024-09-23 17:51:42 +0000

    ok lets go from there huh

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,8 +2,8 @@ import autoprefixer from 'autoprefixer'
 import tailwindcss from 'tailwindcss'
 
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
